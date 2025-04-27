4af109a9e8a66d316edd879f69f1f6d831dbac97
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2025-04-27 15:23:04 +0500

    Add files via upload

---
+++ DIP_7.m
@@ -0,0 +1,66 @@
+I = imread('C:\\Users\\student\\Desktop\\skull.tif');
+
+%I  = rgb2gray(I);
+
+subplot(2,3, 1);
+imshow(I);
+title('Original Image');
+
+
+
+
+%Find edges using the Canny method.
+BW1 = edge(I,'Canny');
+
+subplot(2,3, 2);
+imshow(BW1);
+title('Canny Image');
+
+
+
+
+%Find edges using the Prewitt method.
+
+BW2 = edge(I,'Prewitt');
+
+subplot(2,3, 3);
+imshow(BW2);
+title('Prewitt');
+
+%Sobel : Finds edges at those points where the gradient of the image I is maximum, using the Sobel approximation to the derivative.
+BW3  = edge(I,'Sobel');
+
+subplot(2,3, 4);
+imshow(BW3);
+title('Sobel');
+
+
+%Roberts
+BW4 = edge(I,'Roberts');
+
+subplot(2,3, 5);
+imshow(BW4);
+title('Roberts');
+
+
+%log
+BW5 = edge(I,'log');
+
+subplot(2,3, 6);
+imshow(BW5);
+title('log');
+
+%zerocross
+BW6 = edge(I,'zerocross');
+
+%subplot(2,3, 6);
+%imshow(BW6);
+%title('zerocross');
+
+
+
+
+
+
+%Display both results side-by-side.
+%imshowpair(BW1,BW2,'montage')
