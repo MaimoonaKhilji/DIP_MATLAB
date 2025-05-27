2189765cb3dea04651f9a77a04589139fdc04a90
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2025-05-27 14:35:03 +0500

    dip_3.m

--- dip_3.m
+++ dip_3.m
@@ -1,32 +1,41 @@
+% Agenda
+% Read an Image
+% Apply Logical Operators on image
+
+% Read first image
 f=imread('C:\Users\student\Desktop\a.tif');
 subplot(2,4,1);
 imshow(f);
 title('1st image');
 
-
+% Read second image
 f2=imread('C:\Users\student\Desktop\b.tif');
 subplot(2,4,2);
 imshow(f2);
 title('2nd image');
 
 
-
+% Apply NOT on first image
 subplot(2,4,3);
 imshow(not(f));
 title('NOT 1st image');
 
 
+% Apply NOT on second image
 subplot(2,4,4);
 imshow(not(f2));
 title('NOT 2nd image');
 
 
-
+% Resize image
 f2 = imresize(f2,[1024 1024]);
+
+% AND Operator
 subplot(2,4,5);
 imshow(f&f2);
 title('AND image');
 
+% OR Operator
 subplot(2,4,6);
 imshow(f|f2);
 title('OR image');
